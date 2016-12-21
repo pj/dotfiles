f0946df4e73df81ac60ec79c0cf9f23b5b51e85e
Paul Johnson <paul@johnson.kiwi.nz>
2016-12-21 17:15:11 -0500

    Mute all devices when opening lid to avoid embarrasement.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,4 +1,20 @@
+-- Mute sound when we sleep - avoids problem where system will start playing
+-- sound if headphones are unplugged when sleeping.
 
+function muteOnSleep(event)
+    if event == hs.caffeinate.watcher.systemWillSleep or event == hs.caffeinate.watcher.systemDidWake then
+        for k, device in pairs(hs.audiodevice.allOutputDevices()) do
+            device:setMuted(true)
+        end
+        --device = hs.audiodevice.defaultOutputDevice()
+        --if device then
+        --    hs.alert.show("Muted!")
+        --end
+    end
+end
+
+local watcher = hs.caffeinate.watcher.new(muteOnSleep)
+watcher:start()
 
 function reloadConfig(files)
     doReload = false
