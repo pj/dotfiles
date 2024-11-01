6e1cd23ea83eab583206b6cc280f6a12bacb81a1
Paul Johnson <paul@johnson.kiwi.nz>
2024-10-31 18:26:51 -0700

    wip

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,6 +1,7 @@
 {
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-24.05;
-  inputs.home-manager.url = github:nix-community/home-manager;
+  inputs.home-manager.url = github:nix-community/home-manager?ref=release-24.05;
+
 
   outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations.nixbox = nixpkgs.lib.nixosSystem {
--- nixos/nixbox/configuration.nix
+++ nixos/nixbox/configuration.nix
@@ -2,12 +2,14 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, inputs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
     ];
 
   # Bootloader.
@@ -83,49 +85,129 @@
     description = "paul";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-    #  thunderbird
+      zsh-autosuggestions
+      zsh-fzf-history-search
     ];
+    shell = pkgs.zsh;
+  };
+
+  home-manager.users.paul = {
+    programs.home-manager.enable = true;
+    programs.git = {
+      enable = true;
+      userEmail = "paul@johnson.kiwi.nz";
+      userName = "Paul Johnson";
+      aliases = {
+        gcb = "checkout -b";
+      };
+
+    };
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion = {
+        enable = true;
+      };
+      history = {
+        save = 100000;
+        size = 100000;
+      };
+      initExtraFirst = ''
+        export ZSH_CUSTOM="$HOME/dotfiles/zsh_themes"
+      '';
+      initExtra = ''
+        bindkey -M viins 'kj' vi-cmd-mode
+        export EDITOR=vim
+      '';
+      dotDir = ".config/zsh";
+      oh-my-zsh = {
+        enable = true;
+        theme = "wezm++";
+        plugins = [
+          "vi-mode"
+          "git"
+          "autojump"
+        ];
+      };
+      plugins = [
+        {
+          name = "zsh-autosuggestions";
+          file = "zsh-autosuggestions.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-autosuggestions";
+            rev = "v0.7.0";
+            sha256 = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
+          };
+        }
+
+        {
+          name = "zsh-fzf-history-search";
+          file = "zsh-fzf-history-search.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "joshskidmore";
+            repo = "zsh-fzf-history-search";
+            rev = "master";
+            sha256 = "sha256-tQqIlkgIWPEdomofPlmWNEz/oNFA1qasILk4R5RWobY=";
+          };
+        }
+      ];
+    };
+    programs.autojump.enable = true;
+    programs.fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    home.stateVersion = "24.05";
+    # home.file.".oh-my-zsh/themes".source = ../../zsh_themes;
+    # home.file.".oh-my-zsh/themes".recursive = true;
+    home.file.".local/share/fonts/MonacoNerdFontMono-Regular.ttf".source = ./../../MonacoNerdFontMono-Regular.ttf;
   };
 
   # Install firefox.
-  programs.firefox.enable = true;
+  programs.firefox.enable = false;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  programs.zsh.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-   wget
-alacritty
-vscode
-brave
-git
-(chromium.override { enableWideVine = true;})
-ffmpeg
-btop
-gnomeExtensions.vitals
-python312
-_1password-gui
-_1password
-tmux
+    vim
+    wget
+    alacritty
+    vscode
+    brave
+    git
+    (chromium.override { enableWideVine = true; })
+    ffmpeg
+    btop
+    gnomeExtensions.vitals
+    python312
+    _1password-gui
+    _1password
+    tmux
+    # Needed for nix vscode extension to format properly
+    nixpkgs-fmt
+    zsh
 
   ];
 
-## necessary for arduino development with platformio core
-services.udev.packages = [
-pkgs.platformio-core
-pkgs.openocd
-];
+  ## necessary for arduino development with platformio core
+  services.udev.packages = [
+    pkgs.platformio-core
+    pkgs.openocd
+  ];
 
-# Gigabyte Aorus PRO AX B550i bug - this causes the computer to immediately awake from sleep after suspend
-services.udev.extraRules = lib.strings.concatStringsSep ", " [
+  # Gigabyte Aorus PRO AX B550i bug - this causes the computer to immediately awake from sleep after suspend
+  services.udev.extraRules = lib.strings.concatStringsSep ", " [
     ''ACTION=="add"''
 
     # See below on how to get the correct values for these three
     ''SUBSYSTEM=="pci"''
-    ''ATTR{vendor}=="0x1022"'' 
+    ''ATTR{vendor}=="0x1022"''
     ''ATTR{device}=="0x1483"''
 
     ''ATTR{power/wakeup}="disabled"''
@@ -158,15 +240,14 @@ services.udev.extraRules = lib.strings.concatStringsSep ", " [
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
-# enable nix command and nix flakes
-nix = {
-settings = {
-experimental-features = [ "nix-command" "flakes" ];
-};
-nixPath = [
-      "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
-            "nixos-config=/home/paul/dotfiles/nixos"
-            "/nix/var/nix/profiles/per-user/root/channels"
-];
-};	
+  # enable nix command and nix flakes
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+  };
+
+  fonts.packages = with pkgs; [
+  (nerdfonts.override { fonts = [ "Monaco" ]; })
+];  
 }
