ed4cbcaf567c3d82c31ae43fa4fb11d370eafbb3
Paul Johnson <paul@johnson.kiwi.nz>
2024-11-21 16:22:33 -0800

    WIP

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,29 +1,32 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-24.05;
-  inputs.home-manager.url = github:nix-community/home-manager?ref=release-24.05;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  inputs.home-manager = {
+    url = github:nix-community/home-manager;
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   # Reference an external flake
   inputs.commandline_thing.url = github:pj/commandline_thing?ref=1.0.3;
   inputs.plasma-manager = {
-      url = "github:nix-community/plasma-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
+    url = "github:nix-community/plasma-manager";
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.home-manager.follows = "home-manager";
+  };
 
   outputs = { self, nixpkgs, home-manager, plasma-manager, ... }@inputs: {
     nixosConfigurations.nixbox = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
-      modules = [ 
-        ./nixbox/configuration.nix 
+      modules = [
+        ./nixbox/configuration.nix
         home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
 
-            home-manager.users.paul = import ./nixbox/home.nix;
-          }
-        ];
+          home-manager.users.paul = import ./nixbox/home.nix;
+        }
+      ];
     };
   };
 }
